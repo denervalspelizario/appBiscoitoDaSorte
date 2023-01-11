61e21e59755f4302296f6963bd3f04e4171e1038
denervalspelizario <denervalspelizario@gmail.com>
2023-01-11 11:34:42 -0300

    update app Biscoi da sorte insercaod e imagem biscoito texto e botão principal

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
 import  React  from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';  // 2 TouchableOpacity  tipo de botao facil de customizar
+import ImageOpen from './assets/biscoitoAberto.png';
+import ImageClose from './assets/biscoito.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Image 
+      source={ImageClose}  // 1 chamando a imagem importada
+      style={styles.img}  // 1 estilizacao da imagem
+      
+      />
+
+      <Text>"Alguma Frase Aqui !!!"</Text>
+      <TouchableOpacity>{/* 2 Chamando o botão */}
+        <View>
+          <Text>
+            Quebrar o Biscoito
+          </Text>      
+        </View>            
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: {  // estilizacao do container principal
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  img : {
+    width: 250,
+    height: 250,
+  }
 });
